bde9ac1778c6c8f976420b2e549d5b14a37ff980
Viacheslav <slava.k@ks2corp.com>
2022-12-25 12:31:44 +0300

    cleanup build scripts

--- config/common.cmake
+++ config/common.cmake
@@ -4,11 +4,11 @@ set(APP_FW  app_fw)
 set(STM32F407_TARGET_PATH           ${PROJECT_SOURCE_DIR}/targets/stm32f407)
 set(DESKTOP_TARGET_PATH             ${PROJECT_SOURCE_DIR}/targets/desktop)
 
-set(APP_DIR ${PROJECT_SOURCE_DIR}/app)
-set(DEBUG_DIR ${APP_DIR}/debug)
-set(UTILS_DIR ${APP_DIR}/utils)
-set(IO_DIR ${APP_DIR}/io)
-set(LIBS_DIR ${PROJECT_SOURCE_DIR}/libs)
+set(APP_DIR     ${PROJECT_SOURCE_DIR}/app)
+set(DEBUG_DIR   ${APP_DIR}/debug)
+set(UTILS_DIR   ${APP_DIR}/utils)
+set(IO_DIR      ${APP_DIR}/io)
+set(LIBS_DIR    ${PROJECT_SOURCE_DIR}/libs)
 
 set(APP_DIRS
     ${APP_DIR}
@@ -17,35 +17,8 @@ set(APP_DIRS
     ${IO_DIR}
 )
 
-set(IO_MOCK_DIR ${DESKTOP_TARGET_PATH}/io_mock)
-set(MOCKS_DIR ${DESKTOP_TARGET_PATH}/mocks)
-
-set_property(GLOBAL PROPERTY DESKTOP_INCLUDE_DIR
-    ${DESKTOP_DIR}
-    ${IO_MOCK_DIR}
-    ${MOCKS_DIR}
-    ${APP_DIRS}
-)
-
-# region HAL
-set(HAL_INCLUDE_DIRS
-    # Put here your include dirs, one in each line, relative to CMakeLists.txt file location
-    ${STM32F407_TARGET_PATH}/Core/Inc
-    ${STM32F407_TARGET_PATH}/Drivers/STM32F4xx_HAL_Driver/Inc
-    ${STM32F407_TARGET_PATH}/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy
-    ${STM32F407_TARGET_PATH}/Drivers/CMSIS/Device/ST/STM32F4xx/Include
-    ${STM32F407_TARGET_PATH}/Drivers/CMSIS/Include
-)
-# endregion
-
-# region RTT
-set(RTT_LIB rtt)
-set(RTT_VERSION SEGGER_RTT_V762c)
-set(RTT_DIR ${LIBS_DIR}/${RTT_VERSION})
-set(RTT_INCLUDE_DIRS
-    ${RTT_DIR}/Config
-    ${RTT_DIR}/RTT)
-# endregion
-
 set(COMMON_TYPES_TARGET common_types_test)
-set(TEST_DIR ${PROJECT_SOURCE_DIR}/test)
+set(TEST_DIR            ${PROJECT_SOURCE_DIR}/test)
+
+set(GCC_EMBEDDED    arm-none-eabi-gcc)
+set(GCC_DESKTOP     gcc)
--- config/mcu.cmake
+++ config/mcu.cmake
@@ -1,6 +1,32 @@
 # region MCU GLOBAL PROPERTY
 set(STM32F407F_TARGET               ws407_fw)
 
+# region HAL
+set(HAL_INCLUDE_DIRS
+    # Put here your include dirs, one in each line, relative to CMakeLists.txt file location
+    ${STM32F407_TARGET_PATH}/Core/Inc
+    ${STM32F407_TARGET_PATH}/Drivers/STM32F4xx_HAL_Driver/Inc
+    ${STM32F407_TARGET_PATH}/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy
+    ${STM32F407_TARGET_PATH}/Drivers/CMSIS/Device/ST/STM32F4xx/Include
+    ${STM32F407_TARGET_PATH}/Drivers/CMSIS/Include
+)
+# endregion
+
+# region RTT
+set(RTT_LIB rtt)
+set(RTT_VERSION SEGGER_RTT_V762c)
+set(RTT_DIR ${LIBS_DIR}/${RTT_VERSION})
+set(RTT_INCLUDE_DIRS
+    ${RTT_DIR}/Config
+    ${RTT_DIR}/RTT)
+# Add sources
+file(GLOB_RECURSE RTT_SOURCES
+    ${RTT_DIR}/RTT/*.c
+    ${RTT_DIR}/RTT/*.S
+    ${RTT_DIR}/Syscalls/SEGGER_RTT_Syscalls_GCC.c
+)
+# endregion
+
 # Put here your symbols (preprocessor defines), one in each line
 # Encapsulate them with double quotes for safety purpose
 set(symbols_SYMB
@@ -8,10 +34,6 @@ set(symbols_SYMB
     $<$<CONFIG:RELASE>:"NDEBUG">
     "USE_HAL_DRIVER"
     "STM32F407xx"
-)
-
-set_property(GLOBAL PROPERTY STM32_DEFINES_PROPERTY
-    ${symbols_SYMB}
 )
 
 set(CPU_PARAMETERS -mthumb
@@ -48,11 +70,8 @@ set(STM32F407_LINKER_OPTION
     -specs=nosys.specs
     -u _printf_float                # STDIO float formatting support
     -Wl,--start-group
-    -lc
-    -lm
-    -lnosys
-    -lstdc++
-    -lsupc++
+    -fno-exceptions
+    -fno-rtti
     -Wl,--end-group
     -Wl,--print-memory-usage
 )
