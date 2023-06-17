6cbc6bd5e1ec65badcd6f846106c2dd2b78288f9
Viacheslav <slava.k@ks2corp.com>
2023-06-17 16:08:35 +0300

    added i2c bus

--- targets/stm32f407/Core/Src/main.c
+++ targets/stm32f407/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "i2c.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -93,6 +94,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
   application();
   /* USER CODE END 2 */
